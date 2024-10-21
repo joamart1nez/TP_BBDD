2b41d42c20a9370c2a528ae3cd9c84f82e271ccf
Joaquin <joamartinez2001@gmail.com>
2024-10-21 20:45:11 -0300

    Ultima revision

--- Version Final/CONSULTAS.sql
+++ Version Final/CONSULTAS.sql
@@ -150,17 +150,6 @@ FROM (
 ) AS p
 GROUP BY p.DivisionId,p.Division,P.Equipo
 ORDER BY p.DivisionId 
---SELECT
---    e.Nombre AS Equipo,
---    AVG(CASE WHEN pel.Id_Equipo = e.Id THEN pel.Puntos ELSE pev.Puntos END) AS PromedioPuntos,
---    d.Nombre AS Division
---FROM Partido p
---INNER JOIN PartidoEquipoLocal pel ON p.Id = pel.Id_Partido
---INNER JOIN PartidoEquipoVisitante pev ON p.Id = pev.Id_Partido
---INNER JOIN Equipo e ON (pel.Id_Equipo = e.Id OR pev.Id_Equipo = e.Id)
---INNER JOIN Division d ON e.Id_Division = d.Id
---GROUP BY e.Nombre, d.Nombre,d.Id
---ORDER BY d.Id
 
 --10. Indicar nombre del país y cantidad de jugadores, del país con más jugadores en el torneo.
 SELECT TOP 1 
@@ -173,17 +162,17 @@ ORDER BY CantJugadores DESC
 
 --11. Cantidad de puntos, rebotes totales y asistencias realizados por Kawhi Leonard contra
 --equipos de la otra conferencia.
-SELECT 
-	distinct 
-	(CASE 
-    WHEN te.Descripcion LIKE 'Rebotes%' THEN 'Rebotes Totales'
-    ELSE te.Descripcion
-	END) AS Tipo_Estadistica,
+SELECT DISTINCT 
+	CASE 
+		WHEN te.Descripcion LIKE 'Rebotes%' THEN 'Rebotes Totales'
+		ELSE te.Descripcion
+	END AS Tipo_Estadistica,
 	SUM(EquiposContrarios.Valor) AS TOTAL
 FROM(
 	SELECT 
-	(CASE WHEN pel.Id_Equipo != e.Id then pel.Id_Equipo else pev.Id_Equipo END) AS idContrarios,
-	es.Id_Tipo_Estadistica,es.Valor
+	(CASE WHEN pel.Id_Equipo != e.Id THEN pel.Id_Equipo ELSE pev.Id_Equipo END) AS idContrarios,
+	es.Id_Tipo_Estadistica,
+	es.Valor
 	FROM Jugador AS j
 		INNER JOIN EquipoJugador AS ej ON (j.Id = ej.Id_Jugador AND CONCAT(j.Nombre,' ',j.Apellido)='Kawhi Leonard')
 		INNER JOIN Equipo AS e ON (ej.Id_Equipo = e.Id)
@@ -196,11 +185,9 @@ FROM(
 	INNER JOIN Conferencia AS c ON (d.Id_Conferencia = c.Id AND c.Nombre != 'Oeste')
 	INNER JOIN TipoEstadistica AS te ON (EquiposContrarios.Id_Tipo_Estadistica = te.Id 
 		AND (te.Descripcion LIKE 'Rebotes%' OR te.Descripcion='Puntos' OR te.Descripcion='Asistencias'))
-	GROUP BY CASE 
-		WHEN te.Descripcion LIKE 'Rebotes%' THEN 'Rebotes Totales'
-        ELSE te.Descripcion
-		END
+	GROUP BY CASE WHEN te.Descripcion LIKE 'Rebotes%' THEN 'Rebotes Totales' ELSE te.Descripcion END
 	ORDER BY Tipo_Estadistica
+
 --12. Cantidad de jugadores con más de 15 años de carrera.
 SELECT 
     COUNT(Id) AS CantidadJugadores
@@ -214,52 +201,63 @@ SELECT
 	CONCAT(Subquery.Nombre,' ',Subquery.Apellido) AS NombreCompleto,
 	e.Nombre AS NombreEquipo,
 	ej.NroDorsal AS NroCamiseta
-FROM 
-	(
+FROM (
 	SELECT 
 		j.Id AS IdJugador,
 		j.Nombre,
 		j.Apellido,
 		COUNT(ej.Id_Equipo) AS EquiposPaso 
-	FROM Jugador as j
-		INNER JOIN EquipoJugador AS ej ON (j.Id = ej.Id_Jugador) 
-		GROUP BY j.Id,j.Nombre,j.Apellido)AS Subquery
-	INNER JOIN EquipoJugador AS ej ON (Subquery.IdJugador = ej.Id_Jugador) 
-	INNER JOIN Equipo AS e ON (ej.Id_Equipo=e.Id)
+	FROM Jugador AS j
+	INNER JOIN EquipoJugador AS ej ON (j.Id = ej.Id_Jugador) 
+	GROUP BY j.Id,j.Nombre,j.Apellido
+)AS Subquery
+INNER JOIN EquipoJugador AS ej ON (Subquery.IdJugador = ej.Id_Jugador) 
+INNER JOIN Equipo AS e ON (ej.Id_Equipo=e.Id)
 WHERE EquiposPaso > 1
 
 --14. Cantidad de partidos en que los que al menos un jugador de los Celtics obtuvo más de 25
 --puntos.
 
-SELECT COUNT(idPartidos.Id_Partido) AS CantidadPartidos FROM
-(SELECT DISTINCT Estadistica.Id_Partido FROM Jugador
+SELECT 
+	COUNT(idPartidos.Id_Partido) AS CantidadPartidos 
+FROM (
+	SELECT DISTINCT Estadistica.Id_Partido FROM Jugador
     INNER JOIN EquipoJugador ON (Jugador.Id = EquipoJugador.Id_Jugador)
     INNER JOIN Equipo ON (EquipoJugador.Id_Equipo = Equipo.Id AND Equipo.Nombre = 'Celtics')
     INNER JOIN Estadistica ON (Jugador.Id = Estadistica.Id_Jugador)
     INNER JOIN TipoEstadistica ON (Estadistica.Id_Tipo_Estadistica = TipoEstadistica.Id AND TipoEstadistica.Descripcion = 'Puntos')
-    WHERE Estadistica.Valor > 25) AS idPartidos
+    WHERE Estadistica.Valor > 25
+) AS idPartidos
 
 --15. Indicar ID de partido, fecha, sigla y puntos realizados del equipo local y visitante, del
 --partido en que el equipo de Kawhi Leonard ganó por mayor diferencia de puntos en la
 --temporada
 
-SELECT Subquery.IdPartido, Subquery.Fecha,Subquery.SiglasLocal,Subquery.PuntosLocal,e.Siglas AS SiglasVisitante, Subquery.PuntosVisitante,Subquery.Diferencia FROM(
-SELECT TOP 1
-	p.Id AS IdPartido,
-	p.Fecha,
-	e.Siglas AS SiglasLocal,
-	pel.Puntos AS PuntosLocal,
-	pev.Id_Equipo AS EquipoVisitante,
-	pev.Puntos AS PuntosVisitante,
-	(((CASE WHEN pel.Id_Equipo = e.Id then pel.Puntos else pev.Puntos END) - (CASE WHEN pel.Id_Equipo = e.Id then pev.Puntos else pel.Puntos END))) AS Diferencia 
-FROM Partido AS p
-	INNER JOIN PartidoEquipoLocal pel ON p.Id = pel.Id_Partido
-	INNER JOIN PartidoEquipoVisitante pev ON p.Id = pev.Id_Partido
-	INNER JOIN Equipo e ON (pel.Id_Equipo = e.Id OR pev.Id_Equipo = e.Id)
-	INNER JOIN EquipoJugador AS ej ON (e.Id = ej.Id_Equipo)
-	INNER JOIN Jugador AS j ON (ej.Id_Jugador = j.Id AND CONCAT(j.Nombre,' ',j.Apellido)='Kawhi Leonard')
-WHERE ((CASE WHEN pel.Id_Equipo = e.Id then pel.Puntos else pev.Puntos END) > (CASE WHEN pel.Id_Equipo = e.Id then pev.Puntos else pel.Puntos END))
-GROUP BY p.Id,p.Fecha,e.Siglas,pel.Id_Equipo,pel.Puntos,pev.Id_Equipo,pev.Puntos,e.Id
-ORDER BY Diferencia DESC
+SELECT 
+	Subquery.IdPartido, 
+	Subquery.Fecha,
+	Subquery.SiglasLocal,
+	Subquery.PuntosLocal,
+	e.Siglas AS SiglasVisitante, 
+	Subquery.PuntosVisitante,
+	Subquery.Diferencia 
+FROM(
+	SELECT TOP 1
+		p.Id AS IdPartido,
+		p.Fecha,
+		e.Siglas AS SiglasLocal,
+		pel.Puntos AS PuntosLocal,
+		pev.Id_Equipo AS EquipoVisitante,
+		pev.Puntos AS PuntosVisitante,
+		(((CASE WHEN pel.Id_Equipo = e.Id then pel.Puntos else pev.Puntos END) - (CASE WHEN pel.Id_Equipo = e.Id then pev.Puntos else pel.Puntos END))) AS Diferencia 
+	FROM Partido AS p
+		INNER JOIN PartidoEquipoLocal pel ON p.Id = pel.Id_Partido
+		INNER JOIN PartidoEquipoVisitante pev ON p.Id = pev.Id_Partido
+		INNER JOIN Equipo e ON (pel.Id_Equipo = e.Id OR pev.Id_Equipo = e.Id)
+		INNER JOIN EquipoJugador AS ej ON (e.Id = ej.Id_Equipo)
+		INNER JOIN Jugador AS j ON (ej.Id_Jugador = j.Id AND CONCAT(j.Nombre,' ',j.Apellido)='Kawhi Leonard')
+	WHERE ((CASE WHEN pel.Id_Equipo = e.Id then pel.Puntos else pev.Puntos END) > (CASE WHEN pel.Id_Equipo = e.Id then pev.Puntos else pel.Puntos END))
+	GROUP BY p.Id,p.Fecha,e.Siglas,pel.Id_Equipo,pel.Puntos,pev.Id_Equipo,pev.Puntos,e.Id
+	ORDER BY Diferencia DESC
 ) AS Subquery
 INNER JOIN Equipo e ON (Subquery.EquipoVisitante = e.Id)
